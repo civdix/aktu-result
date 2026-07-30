15913d5fabadb63c1fad724f390f5443f96cdf0d
Shivam Dixit <archanavrindavanvlog@gmail.com>
2026-07-31 01:13:52 +0530

    hh

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
-
 import tailwindcss from '@tailwindcss/vite';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()]
   }
